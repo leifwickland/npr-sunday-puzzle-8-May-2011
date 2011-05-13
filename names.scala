5cbfc84e559ad85df29e3a3895d17580235b5a72
Leif Wickland <leifwickland@gmail.com>
2011-05-13 02:37:46 -0600

    Tidied up and improved perf.

--- names.scala
+++ names.scala
@@ -1,23 +1,14 @@
-import scala.collection.immutable.TreeSet
-import scala.io.Source
-import scala.collection.mutable.HashMap
-import scala.collection.Map
-
 // To solve http://www.npr.org/2011/05/08/136091948/moms-are-downright-excellent
 object names extends App {
-  val names = List[String]() ++ Source.fromFile("names").getLines // Name list grabbed from http://names.mongabay.com/female_names.htm
-  val sevens = names.filter(_.length == 7)
-  val sixes = TreeSet[String]() ++ names.filter(_.length == 6)
+  val names = List[String]() ++ scala.io.Source.fromFile("names").getLines // Name list grabbed from http://names.mongabay.com/female_names.htm
+  val sevens = names.filter(_.length == 7).sorted
+  val sixes = Set[String]() ++ names.filter(_.length == 6)
 
   val matches = 
-    for (s1 <- sevens;
-         s2 <- sevens if (s2 > s1 && s2.startsWith(first4(s1)));
-         p <- tails(s1,s2).permutations if sixes.contains(p))
+    for ((s1, i) <- sevens.zipWithIndex;
+         s2 <- sevens.view(i + 1, sevens.indexWhere(!_.startsWith(s1.substring(0, 4)), i + 1));
+         p <- (s1.substring(4, 7) + s2.substring(4, 7)).permutations if sixes.contains(p))
     yield(List(p, s1, s2))
   val rankedMatches = matches.map(m => (m, (m.map(names.indexOf(_)).sum))).sortBy(_._2)
   rankedMatches.foreach(m => printf("%04d: %s from %s,%s\n", m._2, m._1(0), m._1(1), m._1(2)))
-
-  def first4(s: String) = s.substring(0,4)
-  def last3(s: String) = s.substring(4,7)
-  def tails(a: String, b: String) = last3(a) + last3(b)
 }
